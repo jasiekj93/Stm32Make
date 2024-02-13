2ae6fa612ce2701d53506f2093f6902f2e036991
Adrian Szczepanski <aszczepanski@nodomain.pl>
2024-02-13 14:24:46 +0100

    feat(platform): dodano ArmA7-Linux

---
+++ platform/ArmA7-Linux.mk
@@ -0,0 +1,32 @@
+# ------------------------------------------------
+# @author Adrian Szczepanski
+# @date 17-02-2023
+# ------------------------------------------------
+
+# This file is only a template and should be included 
+# in other Makefile
+
+# Toolchain
+toolchain_prefix := arm-ostl-linux-gnueabi-
+
+# Mcu definition
+cpu := -mcpu=cortex-a7
+fpu := -mfpu=neon-vfpv4
+float-abi := -mfloat-abi=hard
+mcu := $(cpu) -mthumb $(fpu) $(float-abi) --sysroot=$(sysroot)
+
+# Compiler flags
+platform_c_flags := -feliminate-unused-debug-types
+platform_cxx_flags := 
+
+# Debug flags
+platform_debug_flags := -g -gstrict-dwarf 
+
+# Linker flags
+platform_linker_flags := -feliminate-unused-debug-types
+
+# Libraries
+platform_libraries := -lc -lm -lnosys 
+
+# Binary extensions
+platform_binary_extensions := elf hex bin
