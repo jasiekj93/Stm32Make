f97b1224c4da8898c70db41ea336be93416a18d2
Adrian Szczepanski <aszczepanski@nodomain.pl>
2024-02-23 15:33:35 +0100

    feat(Linux): dodano osobne budowanie dtb projektu ze śledzeniem zmian

--- Linux.mk
+++ Linux.mk
@@ -29,9 +29,10 @@ uImage_file := $(artifact_boot_dir)/uImage
 modules_symbols_file := $(artifact_modules_dir)/modules.symbols
 
 device_tree_target := $(patsubst %.dts,%.dtb,$(notdir $(device_tree_source)))
+device_tree_artifact := $(artifact_boot_dir)/$(device_tree_target)
 
 # targets
-.PHONY: init menuconfig build-kernel build-modules build-dtb deploy-kernel deploy-modules deploy-all clean mrproper distclean
+.PHONY: init menuconfig build-kernel build-modules build-dtb deploy-kernel deploy-modules deploy-all deploy-dtb clean mrproper distclean
 
 init: $(config_file)
 
@@ -43,19 +44,14 @@ build-kernel: $(uImage_file)
 
 build-modules: $(modules_symbols_file)
 
-build-dtb: $(uImage_file) 
-	@echo Building device tree
-	+@source $(sdk_enviroment) && \
-		$(MAKE) -C $(source_dir) ARCH=arm dtbs $(device_tree_target) LOADADDR=0xC2000040 O="$(build_dir)"
-	@echo Copying device tree artifacts to $(artifact_boot_dir)
-	@cp $(build_dir)/arch/arm/boot/dts/st*.dtb $(artifact_boot_dir)/
+build-dtb: $(device_tree_artifact) 
 
 deploy-all: deploy-kernel deploy-modules
 
-deploy-kernel: $(uImage_file)
+deploy-kernel: $(uImage_file) 
 	$(call check-hostname)
 	@echo Deploying kernel and device tree to $(hostname)
-	@mv $(artifact_boot_dir)/$(device_tree_target) $(artifact_boot_dir)/device_tree_board_name
+	@mv $(device_tree_artifact) $(artifact_boot_dir)/$(device_tree_board_name)
 	@scp -r $(artifact_boot_dir)/* $(hostname):/boot/
 
 deploy-modules: $(modules_symbols_file)
@@ -63,6 +59,12 @@ deploy-modules: $(modules_symbols_file)
 	@echo Deploying modules to $(hostname)
 	@rm $(artifact_modules_dir)/source $(artifact_modules_dir)/build
 	@scp -r $(artifact_modules_dir)/* root@192.168.7.1:/lib/modules/$(kernel_version)/
+
+deploy-dtb: $(device_tree_artifact) 
+	$(call check-hostname)
+	@echo Deploying project device tree to $(hostname)
+	@mv $(device_tree_artifact) $(artifact_boot_dir)/$(device_tree_board_name)
+	@scp $(artifact_boot_dir)/$(device_tree_board_name) $(hostname):/boot/
 
 $(build_dir) $(artifact_boot_dir):
 	@echo Creating directory $@
@@ -75,19 +77,16 @@ $(source_dir):
 	@for p in `ls -1 $(linux_root)/*.patch`; \
 		do patch -d $@ -p1 < $$p; \
 	done
-ifneq ($(device_tree_source),)
-	@echo Copying project device tree sources
-	@cp $(device_tree_source) $@/arch/arm/boot/dts/
-endif
+
 
 $(config_file): Makefile | $(build_dir) $(source_dir) 
-	source $(sdk_enviroment) && \
-	$(MAKE) -C $(source_dir) ARCH=arm O="$(build_dir)" multi_v7_defconfig fragment*.config
-	for f in `ls -1 $(linux_root)/fragment*.config`; \
+	+@source $(sdk_enviroment) && \
+		$(MAKE) -C $(source_dir) ARCH=arm O="$(build_dir)" multi_v7_defconfig fragment*.config
+	@for f in `ls -1 $(linux_root)/fragment*.config`; \
 		do $(source_dir)/scripts/kconfig/merge_config.sh -m -r -O $(build_dir) $(build_dir)/.config $$f; \
 	done
-	source $(sdk_enviroment) && \
-	(yes '' || true) | $(MAKE) -C $(source_dir) ARCH=arm oldconfig O="$(build_dir)"
+	+@source $(sdk_enviroment) && \
+		(yes '' || true) | $(MAKE) -C $(source_dir) ARCH=arm oldconfig O="$(build_dir)"
 
 $(uImage_file): $(config_file) Makefile | $(artifact_boot_dir) 
 	@echo Building kernel
@@ -97,13 +96,22 @@ $(uImage_file): $(config_file) Makefile | $(artifact_boot_dir)
 	@cp $(build_dir)/arch/arm/boot/uImage $(artifact_boot_dir)/
 	@cp $(build_dir)/arch/arm/boot/dts/st*.dtb $(artifact_boot_dir)/
 
-$(modules_symbols_file): $(uImage_file) Makefile
+$(modules_symbols_file): $(uImage_file) $(config_file) Makefile
 	@echo Building modules
 	+@source $(sdk_enviroment) && \
 		$(MAKE) -C $(source_dir) ARCH=arm modules O="$(build_dir)"
 	@echo Copying modules artifacts to $(artifact_dir)/lib/modules
 	+@source $(sdk_enviroment) && \
 		$(MAKE) -C $(source_dir) ARCH=arm INSTALL_MOD_PATH="$(artifact_dir)" modules_install O="$(build_dir)"
+	
+$(device_tree_artifact) : $(config_file) $(device_tree_source) | $(artifact_boot_dir)
+	@echo Copying project device tree sources
+	@cp $(device_tree_source) $(source_dir)/arch/arm/boot/dts/
+	@echo Building project device tree
+	+@source $(sdk_enviroment) && \
+		$(MAKE) -C $(source_dir) ARCH=arm $(device_tree_target) LOADADDR=0xC2000040 O="$(build_dir)"
+	@echo Copying device tree artifact to $(artifact_boot_dir)
+	@cp $(build_dir)/arch/arm/boot/dts/$(device_tree_target) $(artifact_boot_dir)/
 
 clean: 
 	@echo Cleaning build directory
