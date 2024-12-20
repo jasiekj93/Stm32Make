6d359418b918b5c8aa91b545e9e42632c0b6c7b6
Adrian Szczepanski <aszczepanski@nodomain.pl>
2024-12-20 13:59:58 +0100

    feat: dodano platformę ArmM7-Soft i ArmM4-Soft

---
+++ platform/ArmM4-Soft.mk
@@ -0,0 +1,32 @@
+# ------------------------------------------------
+# @author Adrian Szczepanski
+# @date 17-02-2023
+# ------------------------------------------------
+
+# This file is only a template and should be included 
+# in other Makefile
+
+# Toolchain
+toolchain_prefix := arm-none-eabi-
+
+# Mcu definition
+cpu := -mcpu=cortex-m4
+fpu := -mfpu=fpv4-sp-d16
+float-abi := -mfloat-abi=softfp
+mcu := $(cpu) -mthumb $(fpu) $(float-abi)
+
+# Compiler flags
+platform_c_flags := -fdata-sections -ffunction-sections 
+platform_cxx_flags := -ffreestanding -fno-exceptions -fno-rtti
+
+# Debug flags
+platform_debug_flags := -g -gdwarf-2
+
+# Linker flags
+platform_linker_flags := -specs=nano.specs -specs=nosys.specs -Wl,--print-memory-usage
+
+# Libraries
+platform_libraries := -lc -lm -lnosys 
+
+# Binary extensions
+platform_binary_extensions := elf hex bin
